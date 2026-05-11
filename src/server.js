aae33f0c6c55c0394b8d2eae736732745f60a5cc
ushalalitha <ushalalitha2006@gmail.com>
2026-05-11 17:33:48 +0530

    fixed render port

--- src/server.js
+++ src/server.js
@@ -96,7 +96,7 @@ app.post('/restore', restoreBackup);
 // --------------------
 // START SERVER
 // --------------------
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
